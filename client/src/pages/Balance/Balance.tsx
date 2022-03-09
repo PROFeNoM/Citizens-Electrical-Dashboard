ef1ba461bb0d60c3ccba0bd8d944b590aa265da2
theogom <theo.gom.2000@gmail.com>
2022-03-09 12:29:28 +0100

    Add Balance page structure and the 3 links

---
+++ client/src/pages/Balance/Balance.tsx
@@ -0,0 +1,41 @@
+import './Balance.css';
+import { Header } from '../../containers';
+import { Col, Container, Row } from 'react-bootstrap';
+// import EnergyBalance from '../../components/EnergyBalance/EnergyBalance';
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <>
+            <Header title={'BILAN'} />
+            <h2 id="zone-name">{"<Nom de la zone>"}</h2>
+
+            <Container>
+                <Row>
+                    <Col sm={12} md={12} lg={12} xl={8}>
+                        <div id="balance">
+                            {/*<EnergyBalance />*/}
+                        </div>
+                    </Col>
+                    <Col sm={12} md={12} lg={12} xl={4}>
+                        <div id='nav-items'>
+                            <ul>
+                                <li>
+                                    <Link to={'/production'}>Production</Link>
+                                </li>
+                                <li>
+                                    <Link to={'/consommation'}>Consommation</Link>
+                                </li>
+                                <li>
+                                    <Link to={'/bornes'}>Bornes de recharges</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Home;
