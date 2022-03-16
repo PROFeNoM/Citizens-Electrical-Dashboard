039a349731e4c171ad111727c3f2ef7c2e911605
Clément Saccoccio <clem.saccoccio@protonmail.com>
2022-03-16 23:04:15 +0100

    show data of different urban zones on home page

--- client/src/components/DistrictEnergyBalance/DistrictEnergyBalance.tsx
+++ client/src/components/DistrictEnergyBalance/DistrictEnergyBalance.tsx
@@ -2,62 +2,120 @@ import React from 'react';
 import './DistrictEnergyBalance.css';
 import {
 	Building,
-	getDistrictArea,
-	getDistrictElectricityConsumption,
-	getDistrictElectricityProduction,
-	getDistrictNumberOfBuildings,
-	getDistrictNumberOfSites
+	getAllUrbanZonesName,
+	getUrbanZoneArea,
+	getUrbanZoneElectricityProduction,
+	getUrbanZoneNumberOfBuildings,
+	getUrbanZoneNumberOfSites,
+	getZoneConsumption
 } from '../../scripts/dbUtils';
+import copy from 'fast-copy';
 
-const nbSites: number = getDistrictNumberOfSites(Building.Residential)
-	+ getDistrictNumberOfSites(Building.Professional)
-	+ getDistrictNumberOfSites(Building.Tertiary);
-const nbBuildings: number = getDistrictNumberOfBuildings();
-const area: string = new Intl.NumberFormat().format(Math.round(getDistrictArea()));
+const formatter = new Intl.NumberFormat();
 
-interface State {
-	consumption: string,
-	production: string,
-	ratio: string,
+interface Data {
+	nbOfBuildings: number,
+	area: number,
+	nbOfConsumers: number
+	consumption?: number,
+	production?: number,
 }
 
-export default class DistrictEnergyBalance extends React.Component<{}, State> {
+interface Props {
+	selectedZoneName: string,
+}
+
+interface State {
+	districtData: Data,
+	zonesData: { [name: string]: Data },
+}
+
+export default class DistrictEnergyBalance extends React.Component<Props, State> {
 	constructor(props) {
 		super(props);
 		this.state = {
-			consumption: '...',
-			production: '...',
-			ratio: '...',
+			districtData: {
+				nbOfBuildings: 0,
+				area: 0,
+				nbOfConsumers: 0,
+			},
+			zonesData: {},
 		};
+
+		for (const zoneName of getAllUrbanZonesName()) {
+			this.state.zonesData[zoneName] = {
+				nbOfBuildings: getUrbanZoneNumberOfBuildings(zoneName),
+				area: getUrbanZoneArea(zoneName),
+				nbOfConsumers: getUrbanZoneNumberOfSites(zoneName, Building.Residential)
+					+ getUrbanZoneNumberOfSites(zoneName, Building.Professional)
+					+ getUrbanZoneNumberOfSites(zoneName, Building.Tertiary),
+			};
+
+			this.state.districtData.nbOfBuildings += this.state.zonesData[zoneName].nbOfBuildings;
+			this.state.districtData.area += this.state.zonesData[zoneName].area;
+			this.state.districtData.nbOfConsumers += this.state.zonesData[zoneName].nbOfConsumers;
+		}
 	}
 
 	async componentDidMount() {
 		const t1 = new Date('2021-12-01T00:30:00').getTime();
 		const t2 = new Date('2021-12-31T23:30:00').getTime();
-		const [consumption, production] = await Promise.all([
-			getDistrictElectricityConsumption(t1, Building.All, t2),
-			getDistrictElectricityProduction(t1, t2),
-		]);
-		const formatter = new Intl.NumberFormat();
-		this.setState({
-			consumption: formatter.format(Math.round(consumption / 1_000_000)),
-			production: formatter.format(Math.round(production / 1_000_000)),
-			ratio: Math.round(production / consumption * 100).toString(),
-		});
+
+		let districtConsumption = 0;
+		let districtProduction = 0;
+
+		await Promise.all(getAllUrbanZonesName().map(async zoneName => {
+			const [zoneConsumption, zoneProduction] = await Promise.all([
+				getZoneConsumption(t1, Building.All, zoneName, t2),
+				getUrbanZoneElectricityProduction(t1, zoneName, t2),
+			]);
+
+			this.setState(oldState => {
+				const newState = copy(oldState);
+				newState.zonesData[zoneName].consumption = zoneConsumption;
+				newState.zonesData[zoneName].production = zoneProduction;
+				return newState
+			})
+
+			districtConsumption += zoneConsumption;
+			districtProduction += zoneProduction;
+		}));
+
+		this.setState(oldState => {
+			const newState = copy(oldState);
+			newState.districtData.consumption = districtConsumption;
+			newState.districtData.production = districtProduction;
+			return newState
+		})
+	}
+
+	private get currentData(): Data {
+		return this.props.selectedZoneName !== null
+			? this.state.zonesData[this.props.selectedZoneName]
+			: this.state.districtData;
 	}
 
 	render() {
+		const data = this.currentData;
+		const title = this.props.selectedZoneName ?? 'Quartier de la Bastide';
+		const nbBuildings = formatter.format(data.nbOfBuildings);
+		const area = formatter.format(Math.round(data.area));
+		const nbOfConsumers = formatter.format(data.nbOfConsumers);
+		const consumption = data.consumption !== undefined ? formatter.format(Math.round(data.consumption / 1_000_000)) : '...';
+		const production = data.production !== undefined ? formatter.format(Math.round(data.production / 1_000_000)) : '...';
+		const ratio = data.consumption !== undefined && data.production !== undefined ? formatter.format(Math.round(data.production / data.consumption * 100)) : '...';
+
 		return (
 			<div id="district-info-wrapper">
 				<div id="district-info">
-					<h2 id="district-name">Quartier de la Bastide</h2>
+					<h2 id="district-name">{title}</h2>
 					<ul>
-						<li><span className='data'>{area}</span> m²</li>
 						<li><span className='data'>{nbBuildings}</span> bâtiments</li>
-						<li><span className='data'>{nbSites}</span> consommateurs</li>
-						<li><span className='data'>{this.state.consumption}</span> MWh/mois d'électricité consommée</li>
-						<li><span className='data'>{this.state.production}</span> MWh/mois d'électricité produite</li>
-						<li><span className='data'>{this.state.ratio}</span> % de ratio production/consommation</li>
+						<li><span className='data'>{area}</span> m² de bâtiments</li>
+						<li><span className='data'>{nbOfConsumers}</span> consommateurs</li>
+						<li><span className='data'>{consumption}</span> MWh/mois d'électricité consommée</li>
+						<li><span className='data'>{production}</span> MWh/mois d'électricité produite</li>
+						<li><span className='data'>{ratio}</span> % de ratio production/consommation</li>
 					</ul>
 					<div id="zone-hint">Cliquez sur une zone urbaine pour en savoir plus.</div>
 				</div>
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,18 +3,31 @@ import { Header } from '../../containers';
 import { DistrictEnergyBalance, HomeMap } from '../../components';
 import React from 'react';
 
-function Home() {
-    return (
-        <div id='home-container'>
-            <Header title='ACCUEIL' />
-            <main>
-                <HomeMap
-                    onZoneClick={x => console.log(x)}
-                />
-                <DistrictEnergyBalance/>
-            </main>
-        </div>
-    );
+interface State {
+    selectedZoneName: string | null,
 }
 
-export default Home;
+export default class Home extends React.Component<{}, State>{
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedZoneName: null,
+        };
+    }
+
+    render() {
+        return (
+            <div id='home-container'>
+                <Header title='ACCUEIL'/>
+                <main>
+                    <HomeMap
+                        onZoneClick={zoneName => this.setState({ selectedZoneName: zoneName })}
+                    />
+                    <DistrictEnergyBalance
+                        selectedZoneName={this.state.selectedZoneName}
+                    />
+                </main>
+            </div>
+        );
+    }
+}
