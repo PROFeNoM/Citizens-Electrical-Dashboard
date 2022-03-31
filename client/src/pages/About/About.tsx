f046e40231f0d60aa23b0a9df5dcdcf341839642
theogom <theo.gom.2000@gmail.com>
2022-03-31 16:07:57 +0200

    Clean about page

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
 import './About.css';
 import { Header } from '../../containers';
-import { ChoroplethMap, HorizontalSlider } from '../../components';
 
 function About() {
 	return (
 		<>
-			<Header title={'À PROPOS'}/>
-			<div className='test'>
-				<HorizontalSlider children={(t1, t2) => (
-					<ChoroplethMap t1={t1} t2={t2}/>
-				)}/>
-			</div>
+			<Header title='À propos'/>
 		</>
 	);
 }
