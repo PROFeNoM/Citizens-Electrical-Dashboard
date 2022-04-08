5581484feb0e756511b8c78a22dc98840a74f2f9
Faustin <faustin.boitel@bordeaux-inp.fr>
2022-04-08 10:31:22 +0200

    renaming

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as HamburgerMenu } from './HamburgerMenu/HamburgerMenu'
 export { default as DistrictEnergyBalance } from './DistrictEnergyBalance/DistrictEnergyBalance'
-export { default as UrbanZoneEnergyBalance } from './UrbanZoneEnergyBalance/UrbanZoneEnergyBalance'
 export { default as HomeMap } from './Maps/HomeMap/HomeMap'
 export { default as ChoroplethMap } from './Maps/ChoroplethMap/ChoroplethMap'
 export { default as BaseMap } from './Maps/BaseMap/BaseMap'
