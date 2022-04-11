1af4a0e96ccf2c2b9d3798c07ad87ee21a37c3ad
CHOURA Alexandre <Alexandre.Choura@bordeaux-inp.fr>
2022-04-11 15:50:48 +0200

    Correctly reload production page on dropdown inputs

--- client/src/pages/Production/Production.tsx
+++ client/src/pages/Production/Production.tsx
@@ -3,8 +3,6 @@ import React, {useState} from "react";
 import {ProductionChoroplethMap, WeeklyProduction} from '../../components';
 import {useParams} from 'react-router-dom';
 import HeaderDropDown from '../../containers/HeaderDropDown/HeaderDropDown';
-import {Menu, MenuItem, Button} from '@mui/material';
-import {ProducerProfile} from '../../scripts/api';
 import {Link} from 'react-router-dom';
 import chart_ic from '../../images/chart_icon.png';
 import i_ic from '../../images/i_icon.png';
@@ -20,38 +18,39 @@ function Production() {
 	return (
 		<>
 			<HeaderDropDown/>
-			<div id="production-content" key={window.location.pathname}/>
+			<div id="production-content" key={window.location.pathname}>
 
-			<div id="graph-container">
-				<div id="choropleth-map-wrapper">
-					<ProductionChoroplethMap t1={t1} t2={t2}/>
-				</div>
+				<div id="graph-container">
+					<div id="choropleth-map-wrapper">
+						<ProductionChoroplethMap t1={t1} t2={t2}/>
+					</div>
 
-				<div key={currentChart} id="chart-wrapper">
-					<WeeklyProduction
-						t1={t1}
-						t2={t2}
-						urbanZone={params.zoneName}
-						title={'Production hebdomadaire de la zone urbaine par rapport au quartier'}
-					/>
-					<SolarDonut
-						t1={t1}
-						t2={t2}
-						urbanZone={params.zoneName}
-						title={'Répartition des productions solaires'}
-					/>
-					<>
-						<div className="controls">
-							{/*<div onClick={this.props.onCancel}>retour</div>-->*/}
-							<img id="icons" src={chart_ic} alt="Pictogramme de graphique"/>
-							<Link to={'/consommation/' + params.zoneName}>consommation</Link>
-							<Link to={'/production/' + params.zoneName}>production</Link>
-						</div>
-						<div className="controls">
-							<img id="icons" src={i_ic} alt="Pictogramme d'information"/>
-							<Link to={'/bornes/' + params.zoneName}>bornes de recharges</Link>
-						</div>
-					</>
+					<div key={currentChart} id="chart-wrapper">
+						<WeeklyProduction
+							t1={t1}
+							t2={t2}
+							urbanZone={params.zoneName}
+							title={'Production hebdomadaire de la zone urbaine par rapport au quartier'}
+						/>
+						<SolarDonut
+							t1={t1}
+							t2={t2}
+							urbanZone={params.zoneName}
+							title={'Répartition des productions solaires'}
+						/>
+						<>
+							<div className="controls">
+								{/*<div onClick={this.props.onCancel}>retour</div>-->*/}
+								<img id="icons" src={chart_ic} alt="Pictogramme de graphique"/>
+								<Link to={'/consommation/' + params.zoneName}>consommation</Link>
+								<Link to={'/production/' + params.zoneName}>production</Link>
+							</div>
+							<div className="controls">
+								<img id="icons" src={i_ic} alt="Pictogramme d'information"/>
+								<Link to={'/bornes/' + params.zoneName}>bornes de recharges</Link>
+							</div>
+						</>
+					</div>
 				</div>
 			</div>
 		</>
