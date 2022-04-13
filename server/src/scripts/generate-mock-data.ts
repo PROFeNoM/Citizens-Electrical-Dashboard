af70c3f1cdb6104ea28f5f745ffd1d762bcc3259
Clément Saccoccio <clem.saccoccio@protonmail.com>
2022-04-13 12:15:53 +0200

    compute zone from addresses and coefficients from geodata

--- server/src/scripts/generate-mock-data.ts
+++ server/src/scripts/generate-mock-data.ts
@@ -1,12 +1,18 @@
-import { mkdir } from 'fs/promises';
+import * as turf from '@turf/turf';
 import * as download from 'download';
 import { logger } from '../logger';
-import { format, parseString } from 'fast-csv';
+import { parseFile, parseString, writeToPath } from 'fast-csv';
 import * as FormData from 'form-data';
 import axios from 'axios';
 import { EOL } from 'os';
-import { createWriteStream } from 'fs';
-import { exists, mockSrcDataDir } from './utils';
+import { exists, mockSrcDataDir, readCsv } from './utils';
+import { ConsumerProfile } from '../db/entities/Consumption';
+import { ProducerProfile } from '../db/entities/Production';
+import * as zones from '../geodata/zones.json'
+import { FeatureCollection, MultiPolygon } from '@turf/turf';
+import { mkdir } from 'fs/promises';
+
+type Coefficients = Record<string, Record<ConsumerProfile | ProducerProfile, number>>;
 
 const timestampStart = '2021-06-01T00:00:00Z';
 const timestampEnd = '2021-12-31T23:30:00Z';
@@ -16,7 +22,9 @@ main().catch(console.error);
 async function main() {
 	await mkdir(mockSrcDataDir, { recursive: true });
 	await downloadConsAndProdData();
-	await downloadRandomAddresses();
+	const addresses = await downloadRandomAddresses();
+	const coefficients = getCoefficients();
+	await generateMock(addresses, coefficients);
 }
 
 async function downloadConsAndProdData() {
@@ -30,7 +38,6 @@ async function downloadConsAndProdData() {
 	await downloadDataset('prod-region', 'production');
 
 	async function downloadDataset(datasetName: string, destName: string) {
-
 		if (await exists(`${mockSrcDataDir}/${destName}.csv`)) {
 			logger.info('-> file already exists, skipping')
 			return
@@ -46,18 +53,27 @@ async function downloadConsAndProdData() {
 	}
 }
 
-async function downloadRandomAddresses() {
+async function downloadRandomAddresses(): Promise<string[][]> {
 	logger.info(`downloading random addresses`);
 
-	if (await exists(mockSrcDataDir + '/addresses.csv')) {
+	const filename = mockSrcDataDir + '/addresses.csv';
+
+	// return data from cache if present
+	if (await exists(filename)) {
 		logger.info('-> file already exists, skipping')
-		return
+		const rows: string[][] = [];
+		for await (const row of parseFile(filename, { delimiter: ';', headers: true })) {
+			rows.push(row);
+		}
+		return rows;
 	}
+
+	// generate a CSV containing a mesh of points
 
 	const NW = [44.85167061339378, -0.5671098076715935];
 	const SE = [44.83706864026471, -0.5441770324348592];
 
-	const n = 25;
+	const n = 45;
 	const latStep = (SE[0] - NW[0]) / n;
 	const longStep = (SE[1] - NW[1]) / n;
 
@@ -68,6 +84,8 @@ async function downloadRandomAddresses() {
 		}
 	}
 
+	// transform the mesh of points to a list of addresses using the address API
+
 	const form = new FormData();
 	form.append('data', csv, { filename: 'data.csv' });
 	const res = await axios.post('https://api-adresse.data.gouv.fr/reverse/csv', form, { headers: form.getHeaders() })
@@ -76,12 +94,58 @@ async function downloadRandomAddresses() {
 		.split('\n')
 		.join(EOL), { headers: true, delimiter: ';' });
 
-	const outStream = format({ delimiter: ';' });
-	outStream.pipe(createWriteStream(mockSrcDataDir + 'addresses.csv'));
+	// filter out unnecessary fields and add the name of the zone where the point is
+
+	const filtered: string[][] = [];
 
 	for await (const a of addresses) {
-		outStream.write([a.result_housenumber, a.result_name, a.result_citycode]);
+		// find in which zone is the address
+		let zoneName: string = null;
+		const point = turf.point([a.longitude, a.latitude]);
+		for (const zone of (zones as FeatureCollection<MultiPolygon>).features) {
+			if (turf.booleanContains({ type: 'Polygon', coordinates: zone.geometry.coordinates[0] }, point)) {
+				zoneName = zone.properties.libelle;
+				break;
+			}
+		}
+		if (zoneName === null) {
+			continue;
+		}
+
+		filtered.push([zoneName, a.result_housenumber, a.result_name, a.result_citycode]);
 	}
 
-	outStream.end();
+	// cache the result in a file
+	writeToPath(filename, filtered, {
+		delimiter: ';',
+		headers: ['zone name', 'house number', 'street name', 'INSEE city code']
+	});
+
+	return filtered;
+}
+
+/**
+ * Returns, for each zone and for each profile, a coefficient to apply on mock production and consumption
+ * data in order to have something that seems legit.
+ */
+function getCoefficients(): Coefficients {
+	const coefficients: Coefficients = {};
+
+	for (const zone of (zones as FeatureCollection).features) {
+		const coeff = coefficients[zone.properties.libelle] = {} as Record<string, number>;
+		coeff[ConsumerProfile.PROFESSIONAL] = zone.properties.PRO1 + zone.properties.PRO2;
+		coeff[ConsumerProfile.RESIDENTIAL] = zone.properties.RES1 + zone.properties.RES2;
+		coeff[ConsumerProfile.TERTIARY] = zone.properties.ENT;
+		coeff[ConsumerProfile.PUBLIC_LIGHTING] = Math.round(turf.area(zone) / 10_000);
+		coeff[ProducerProfile.SOLAR] = zone.properties.PROD_F5;
+	}
+
+	return coefficients;
+}
+
+async function generateMock(addresses: string[][], coefficients: Coefficients) {
+
+	for await (const line of readCsv(mockSrcDataDir + '/consumption-part1.csv')) {
+
+	}
 }
