92ac735e625b88a799165a9355d21713a65ca315
theogom <theo.gom.2000@gmail.com>
2022-04-30 15:49:56 +0200

    Better default parameters on base map, disable interactivity and enable auto resizing for responsiveness

--- client/src/components/Maps/BaseMap/BaseMap.tsx
+++ client/src/components/Maps/BaseMap/BaseMap.tsx
@@ -1,36 +1,47 @@
 import React, { MutableRefObject } from 'react';
+
 import { LngLatBoundsLike, LngLatLike, Map } from 'mapbox-gl';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoicHJvZmVub20iLCJhIjoiY2wwNDR3NnNoMGE2NTNpb2dxazg4NW1tdCJ9.hCeP49duNV1x-13qb2aMtA';
-const defaultCenter: LngLatLike = [-0.5562, 44.8449];
+const defaultCenter: LngLatLike = [-0.5564, 44.8431];
+const defaultBounds: LngLatBoundsLike = [[-0.5463, 44.8522], [-0.5665, 44.8382]];
 
 export interface BaseMapProps {
-	center?: LngLatLike,
-	/** defines the zone to be displayed (setting this will disable interactivity) */
-	bounds?: LngLatBoundsLike,
-	zoom?: number,
-	pitch?: number,
+	center?: LngLatLike;
+	bounds?: LngLatBoundsLike;
+	zoom?: number;
+	pitch?: number;
 }
 
+/**
+ * Base map
+ * 
+ * Map centered on the center of the Bastide neighborhood.
+ * Fit the bounds when the window is resized.
+ * Interactivity is disabled.
+ */
 export default class BaseMap extends React.Component<BaseMapProps, {}> {
 	private mapContainerRef: MutableRefObject<HTMLDivElement> = React.createRef();
 	private mapRef: MutableRefObject<Map> = React.createRef();
+
+	get map() {
+		return this.mapRef.current;
+	}
 
 	componentDidMount() {
 		this.mapRef.current = new Map({
 			container: this.mapContainerRef.current,
 			style: 'mapbox://styles/mapbox/streets-v9',
 			accessToken: MAPBOX_TOKEN,
-			zoom: this.props.zoom ?? 13.5,
+			zoom: this.props.zoom ?? 15.5,
 			center: this.props.center ?? defaultCenter,
-			pitch: this.props.pitch ?? 0,
-			interactive: !this.props.bounds,
+			pitch: this.props.pitch ?? 42,
+			interactive: false,
 		});
 
-		if (this.props.bounds) {
-			this.fitBounds(false);
-			this.map.on('resize', () => this.fitBounds(true));
-		}
+		this.fitBounds(false);
+		// Resize map when window is resized
+		this.map.on('resize', () => this.fitBounds(true));
 	}
 
 	ensureMapLoading(): Promise<void> {
@@ -40,24 +51,20 @@ export default class BaseMap extends React.Component<BaseMapProps, {}> {
 			} else {
 				this.map.on('load', resolve);
 			}
-		})
+		});
+	}
+
+	private fitBounds(animate: boolean) {
+		this.map.fitBounds(this.props.bounds ?? defaultBounds, {
+			center: this.props.center ?? defaultCenter,
+			animate: animate,
+			padding: -80,
+		});
 	}
 
 	render() {
 		return (
 			<div className="map-container" ref={this.mapContainerRef} />
 		);
-	}
-
-	get map() {
-		return this.mapRef.current;
-	}
-
-	private fitBounds(animate: boolean) {
-		this.map.fitBounds(this.props.bounds, {
-			center: this.props.center ?? defaultCenter,
-			animate: animate,
-			padding: -80,
-		})
 	}
 }
