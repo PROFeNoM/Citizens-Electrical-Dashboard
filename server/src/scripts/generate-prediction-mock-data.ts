ff884894a040dbf6c27a7ecf71591af31c70f5e7
CHOURA Alexandre <Alexandre.Choura@bordeaux-inp.fr>
2022-05-01 10:32:42 +0200

    Adapt current code to generate prediction mock data
    Former-commit-id: 2249651082b78619c2faf09847ee3b230e5404d7

---
+++ server/src/scripts/generate-prediction-mock-data.ts
@@ -0,0 +1,24 @@
+import {downloadRandomAddresses, generateMock, getCoefficients, readCsv} from "./utils";
+import {logger} from "../logger";
+
+// Usage:
+// node generate-prediction-mock-data.js <srcPath> <destPath> <consumption|production>
+
+// Retrieve the command line arguments
+const srcPath = process.argv[2];
+const destPath = process.argv[3];
+
+main().catch(console.error);
+
+async function main() {
+	const addresses = await downloadRandomAddresses();
+	const coefficients = getCoefficients();
+	await generateMock(addresses, coefficients, destPath, predictionIterator);
+	logger.info('done!');
+}
+
+async function* predictionIterator(): AsyncGenerator<Record<string, string>> {
+	for await (const line of readCsv(srcPath, ',')) {
+		yield line;
+	}
+}
