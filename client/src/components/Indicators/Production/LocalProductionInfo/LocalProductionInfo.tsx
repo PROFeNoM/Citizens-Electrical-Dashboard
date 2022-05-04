91cc4af8a111c0f219c00dda0756c32018927873
theogom <theo.gom.2000@gmail.com>
2022-05-04 16:39:37 +0200

    Improve production info indicator
    Custom text when there is no production points
    
    
    Former-commit-id: adee765a576a3c7535e6cee48a401c591fc72ee1

--- client/src/components/Indicators/Production/LocalProductionInfo/LocalProductionInfo.tsx
+++ client/src/components/Indicators/Production/LocalProductionInfo/LocalProductionInfo.tsx
@@ -5,11 +5,12 @@ import React from 'react';
 import { ProducerProfile } from 'constants/profiles';
 import { getTotalProduction } from 'scripts/api';
 import { zonesGeoJSON } from 'geodata';
+import { wattsToKilowatts } from 'scripts/utils';
 
 interface Props {
+	zoneName: string;
 	t1: number;
 	t2: number;
-	urbanZone: string;
 }
 
 interface State {
@@ -32,12 +33,12 @@ export default class LocalProductionInfo extends React.Component<Props, State> {
 	}
 
 	async fetchData() {
-		const { t1, t2, urbanZone } = this.props;
+		const { t1, t2, zoneName } = this.props;
 		// Retreive the number of production points using geodata zones
-		const zone = zonesGeoJSON.features.find(z => z.properties.libelle === urbanZone).properties.PROD_F5;
-		const totalProduction = await getTotalProduction(t1, t2, [ProducerProfile.SOLAR], urbanZone);
-		//const bestDay = await getBestDay(t1, t2, [ProducerProfile.SOLAR], urbanZone);
-		//const worstDay = await getWorstDay(t1, t2, [ProducerProfile.SOLAR], urbanZone);
+		const zone = zonesGeoJSON.features.find(z => z.properties.libelle === zoneName).properties.PROD_F5;
+		const totalProduction = await getTotalProduction(t1, t2, [ProducerProfile.SOLAR], zoneName);
+		//const bestDay = await getBestDay(t1, t2, [ProducerProfile.SOLAR], zoneName);
+		//const worstDay = await getWorstDay(t1, t2, [ProducerProfile.SOLAR], zoneName);
 		this.setState({
 			productionPoints: zone,
 			totalProduction: totalProduction,
@@ -48,24 +49,66 @@ export default class LocalProductionInfo extends React.Component<Props, State> {
 
 	async componentDidMount() {
 		// TODO: check, often error here
-		await this.fetchData();
+		try {
+			await this.fetchData();
+		}
+		catch (e) {
+			console.error('Error while fetching data', e);
+		}
 	}
 
-	/*async componentDidUpdate() {
-		await this.fetchData();
-	}*/
+	async componentDidUpdate(prevProps: Props) {
+		if (prevProps.zoneName === this.props.zoneName && prevProps.t1 === this.props.t1 && prevProps.t2 === this.props.t2) {
+			return;
+		}
+
+		try {
+			await this.fetchData();
+		}
+		catch (e) {
+			console.error('Error while fetching data', e);
+		}
+	}
 
 	render() {
-		const plural: boolean = this.state.productionPoints > 1;
-		const district: boolean = this.props.urbanZone === null;
+		const { zoneName } = this.props;
+		const { productionPoints, totalProduction } = this.state;
+		const district: boolean = zoneName === null;
+		const formatter = new Intl.NumberFormat('fr-FR', { style: 'decimal', maximumFractionDigits: 2 });
+
+		const totalProductionKilowatts = formatter.format(wattsToKilowatts(totalProduction));
+		const CO2avoided = formatter.format(totalProduction * 0.0025);
+
+		if (productionPoints === 0) {
+			return (
+				<div id="local-production-info" className="text-indicator">
+					<p>
+						{district ? 'Le quartier' : 'La zone'} <strong>{district ? 'Bastide' : zoneName}</strong> n'a pas de points de production solaire.
+					</p>
+				</div>
+			);
+		}
+
+		if (totalProduction === 0) {
+			return (
+				<div id="local-production-info" className="text-indicator">
+					<p>
+						{district ? 'Le quartier' : 'La zone'} <strong>{district ? 'Bastide' : zoneName}</strong> possède {productionPoints > 1 ? <strong>{productionPoints} points</strong> : 'un point'} de production solaire.
+					</p>
+					<p>
+						Il n'y a pas de production électrique pendant la période.
+					</p>
+				</div>
+			);
+		}
 
 		return (
 			<div id="local-production-info" className="text-indicator">
 				<p>
-					La production d'énergie solaire {plural ? 'des ' : ''}{plural ? <strong>{this.state.productionPoints} points</strong> : 'du point'} de production {district ? 'du quartier' : 'de la zone'} <strong>{district ? 'Bastide' : this.props.urbanZone}</strong> s'élève à <strong>{new Intl.NumberFormat().format(this.state.totalProduction)}&nbsp;kWh</strong>.
+					La production d'énergie eléctrique {productionPoints > 1 ? 'des ' : ''}{productionPoints > 1 ? <strong>{productionPoints} points</strong> : 'du point'} de production solaire {district ? 'du quartier' : 'de la zone'} <strong>{district ? 'Bastide' : this.props.zoneName}</strong> s'élève à <strong>{totalProductionKilowatts}&nbsp;kWh</strong>.
 				</p>
 				<p>
-					Cette production a permis d'éviter l'émission de <strong>{Math.round(29 * this.state.totalProduction / 1000 / 1000 / 1000)}&nbsp;kilotonnes</strong> de CO2 lors de cette période.
+					Cette production a permis d'éviter l'émission de <strong>{CO2avoided}&nbsp;kilotonnes</strong> de CO2 lors de cette période.
 				</p>
 			</div>
 		);
