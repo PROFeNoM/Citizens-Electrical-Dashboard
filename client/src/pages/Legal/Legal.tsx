3d15decef59ac1a23d62cbb099af99bff0c0ffd0
theogom <theo.gom.2000@gmail.com>
2022-05-08 02:46:24 +0200

    Typo in Legal page
    Former-commit-id: c335f7704ca80c41db71a812047fa7d129998d2f

--- client/src/pages/Legal/Legal.tsx
+++ client/src/pages/Legal/Legal.tsx
@@ -11,7 +11,7 @@ function Legal() {
 			</header>
 			<main>
 				<div id="legal-text-wrapper">
-					<h2>Propriété intellectuelle</h2>
+					<h2>Propriété intelectuelle</h2>
 
 					<p>
 						Selon l'accord oral des clients, les sources du logiciel sont disponibles sous une licence open source au choix de l'équipe de développement. L'ensemble des sources du logiciel est donc licencié sous la <a href="https://www.gnu.org/licenses/gpl-3.0.fr.html" target={'blank'}>GNU General Public License 3</a>.
